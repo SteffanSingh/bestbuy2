94d4322a7ef559f0dadfc3ab300732efaa5a15bb
unknown <samratkiev@gmail.com>
2023-06-06 23:26:34 +0100

    funciton created

---
+++ test_product.py
@@ -0,0 +1,24 @@
+
+
+
+def test_creating_prod():
+    pass
+
+def test_creating_prod_invalid_details():
+    pass
+
+def test_prod_become_inactive():
+    pass
+
+def test_buy_modifies_quantity():
+    pass
+
+def test_buy_too_much():
+    pass
+
+
+
+
+
+
+
